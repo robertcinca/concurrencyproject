094911f55c4acb25df656217ad6ff598d813e3a6
Lorenz Friedrich <Lorenz@Lorenzs-MacBook-Pro.local>
2016-11-22 09:59:34 +0800

    Solved the barrierproblem. Now the program should really be fully functional. Comments are still missing and some of the methods are too overloaded and a clusterfuck because i added and took away operations all the time. some cleaning up is necessary, the code could look a lot nicer than how it does right now.

--- src/v1/Employee.java
+++ src/v1/Employee.java
@@ -1,5 +1,7 @@
 package v1;
 
+import java.util.concurrent.TimeUnit;
+
 //all the actions in this task should return a value to teller instead of running in here. this makes it possible to implement cyclic barrier
 
 public class Employee {
@@ -15,17 +17,30 @@ public class Employee {
 
 	public void doTask(int transactionType, int amount, int admitted, int timer, int arrived, int processingTime, BankStaff teller) {
 		if(transactionType == 1) {
-			deposit(amount, processingTime, admitted, teller, timer, arrived);
+			deposit(amount, processingTime, admitted, teller, arrived);
 		} else if(transactionType == 2) {
-			withdraw(amount, processingTime, admitted, teller, timer, arrived);
+			withdraw(amount, processingTime, admitted, teller, arrived);
 		} else {
-			checkBalance(admitted, processingTime, teller, timer, arrived);
+			checkBalance(admitted, processingTime, teller, arrived);
 		}
 	}
 	
-	private void deposit(int amount, int processingTime, int admitted, BankStaff teller, int timer, int arrived) {
+	private void deposit(int amount, int processingTime, int admitted, BankStaff teller, int arrived) {
 		//there is a problem when employees are waiting to acquire a lock, they are not passing barriers
-		employer.getLock().writeLock().lock();
+		while(true) {
+			try {
+				if(employer.getLock().writeLock().tryLock(0, TimeUnit.SECONDS)) {
+					break;
+				} else {
+					teller.awaitBarrier(1);
+					teller.awaitBarrier(2);
+					teller.awaitBarrier(3);
+					teller.awaitBarrier(4);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		while(teller.getEmployer().getTimer() < (admitted +processingTime)) {
 			teller.awaitBarrier(1);
 			teller.awaitBarrier(2);
@@ -33,13 +48,26 @@ public class Employee {
 			teller.awaitBarrier(4);
 		}
 		employer.setBalance(employer.getBalance() + amount);
-		System.out.println("(" +timer+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", deposits "  +amount+
+		System.out.println("(" +teller.getEmployer().getTimer()+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", deposits $"  +amount+
 				" into " +employer+ ". Admitted at time " +admitted+ ", arrived at " +arrived);
 		employer.getLock().writeLock().unlock();
 	}
 	
-	private void withdraw(int amount, int processingTime, int admitted, BankStaff teller, int timer, int arrived) {
-		employer.getLock().writeLock().lock();
+	private void withdraw(int amount, int processingTime, int admitted, BankStaff teller, int arrived) {
+		while(true) {
+			try {
+				if(employer.getLock().writeLock().tryLock(0, TimeUnit.SECONDS)) {
+					break;
+				} else {
+					teller.awaitBarrier(1);
+					teller.awaitBarrier(2);
+					teller.awaitBarrier(3);
+					teller.awaitBarrier(4);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		while(teller.getEmployer().getTimer() < (admitted +processingTime)) {
 			teller.awaitBarrier(1);
 			teller.awaitBarrier(2);
@@ -47,21 +75,34 @@ public class Employee {
 			teller.awaitBarrier(4);
 		}
 		employer.setBalance(employer.getBalance() - amount);
-		System.out.println("(" +timer+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", withdraws " +amount+
+		System.out.println("(" +teller.getEmployer().getTimer()+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", withdraws $" +amount+
 				" from " +employer+ ". Admitted at time " +admitted+ ", arrived at " +arrived);
 		employer.getLock().writeLock().unlock();
 	}
 	
-	public void checkBalance(int admitted, int processingTime, BankStaff teller, int timer, int arrived) {
-		employer.getLock().readLock().lock();
+	public void checkBalance(int admitted, int processingTime, BankStaff teller, int arrived) {
+		while(true) {
+			try {
+				if(employer.getLock().readLock().tryLock(0, TimeUnit.SECONDS)) {
+					break;
+				} else {
+					teller.awaitBarrier(1);
+					teller.awaitBarrier(2);
+					teller.awaitBarrier(3);
+					teller.awaitBarrier(4);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		while(teller.getEmployer().getTimer() < (admitted +processingTime)) {
 			teller.awaitBarrier(1);
 			teller.awaitBarrier(2);
 			teller.awaitBarrier(3);
 			teller.awaitBarrier(4);
 		}
-		System.out.println("(" +timer+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", checks balance of " +employer+
-				". It is " +employer.getBalance()+ ". Admitted at time " +admitted+ ", arrived at " +arrived);
+		System.out.println("(" +teller.getEmployer().getTimer()+ ") Employee " +employeeID+ ", with help of Teller " +teller.getID()+ ", checks balance of " +employer+
+				". It is $" +employer.getBalance()+ ". Admitted at time " +admitted+ ", arrived at " +arrived);
 		employer.getLock().readLock().unlock();
 	}
 	
