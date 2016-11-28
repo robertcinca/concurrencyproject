1b8a833b5fc5f1f6cf3431aa3ebdc5a1bd03ebb8
Lorenz Friedrich <Lorenz@Lorenzs-MacBook-Pro.local>
2016-11-28 11:06:14 +0800

    short instruction in  main

--- src/v1/Main.java
+++ src/v1/Main.java
@@ -2,6 +2,12 @@ package v1;
 
 import java.awt.EventQueue;
 
+/**
+ * Read Documentation for Execution Details.
+ * For proper execution, this project has to contain:
+ * - src folder with v1 package; should contain 8 classes
+ * - resources folder with 3 configs, named config1.txt, config2.txt, config3.txt
+ */
 public class Main {
 	
 	public static void main(String[] args) {
